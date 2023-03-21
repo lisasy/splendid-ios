ba3e57753e3b1ccf8dd5e28d87942c106eeb427f
Lisa Sy <lisasyis@gmail.com>
2023-03-20 20:48:02 -0700

    Initial Commit

---
+++ splendid-v2/splendid_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  splendid_v2App.swift
+//  splendid-v2
+//
+//  Created by Lisa Sy on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct splendid_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
